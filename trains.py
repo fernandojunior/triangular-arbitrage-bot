26b5ef1731e9d58e6b40422f991eaa2207d9509f
Fernando Felix <fernandojr.ifcg@live.com>
2018-05-24 02:17:10 -0300

    Improve shortest_route_distance to handle endless routes

--- trains.py
+++ trains.py
@@ -5,6 +5,9 @@ author: Fernando Felix do Nascimento Junior
 year: 2017
 """
 from collections import defaultdict
+import operator
+
+ENDLESS_ROUTE_PATH_ERROR = 'ENDLESS_ROUTE_PATH_ERROR'
 
 
 class Graph:
@@ -21,6 +24,10 @@ class Graph:
     def distance(self, from_node, to_node):
         """ Return a distance between two nodes """
         return self.distances[(from_node, to_node)]
+
+    def get_neighbor_distances(self, from_node):
+        """ Return the node neighboor distances """
+        return {to_node: self.distance(from_node, to_node) for to_node in self.edges[from_node]}
 
     def add_edges(self, node_pair_distance_arrs):
         """ Add edges from an array of node_pair_distances array: [from node, to_node, distance] """
@@ -48,40 +55,64 @@ def route_distance(graph, route, exception_message="NO SUCH ROUTE"):
     except:
         return exception_message
 
+def find_nearest_neighbor(graph, node):
+    ''' Return the nearest neighbor of a node '''
+    min_distance = float('Inf')
+    nearest = None
 
-def shortest_route_distance(graph, start_node, end_node):
+    for neighbor in graph.edges[node]:
+        distance = graph.distance(node, neighbor)
+        if distance < min_distance:
+            min_distance = distance
+            nearest = neighbor
+
+    return nearest, min_distance
+
+def sort_neighbor_distances(graph, node, reverse=False):
+    ''' Return the sorted neighbor distances of a node '''
+    neighbor_distances = graph.get_neighbor_distances(node).items()
+    sorted_neighbor_distances = sorted(neighbor_distances, key=operator.itemgetter(1), reverse=reverse)
+
+    return sorted_neighbor_distances
+
+def shortest_route_distance(graph, start_node, end_node, current_node=None, route="", total_distance=0, max_stops=3):
     """
     Giving a distance graph, return the shortest route distance between a starting and ending routes without repetition.
     It's based on nearest neighbour algorithm. https://en.wikipedia.org/wiki/Nearest_neighbour_algorithm
     """
-    route = ""
-    total_distance = 0
-
-    current_node = start_node
+    current_node = current_node or start_node
     route = route + current_node
 
-    def nearest_neighbor(node):
-        ''' Return the nearest neighbor of a node '''
-        min_distance = float('Inf')
-        nearest = None
+    print('init', route, start_node, end_node, current_node)
+    sorted_neighbor_distances = sort_neighbor_distances(graph, current_node, reverse=False)
 
-        for neighbor in graph.edges[node]:
-            distance = graph.distance(node, neighbor)
-            if distance < min_distance:
-                min_distance = distance
-                nearest = neighbor
+    if len(route) > 2 and current_node in [start_node, end_node]:
+        return { 'route': route, 'distance': total_distance }
 
-        return nearest, min_distance
+    print(sorted_neighbor_distances)
 
-    while True:
-        nearest_node, nearest_distance = nearest_neighbor(current_node)
-        route = route + nearest_node
-        total_distance = total_distance + nearest_distance
-        current_node = nearest_node
-        if current_node is end_node:
-            break
+    for neighbor, distance in sorted_neighbor_distances:
+        if (len(route) > max_stops):
+            continue
 
-    return total_distance
+        if (neighbor not in [start_node, end_node] and neighbor in route):
+            continue
+
+        current_node = neighbor
+
+        result = shortest_route_distance(
+            graph,
+            start_node,
+            end_node,
+            current_node=current_node,
+            route=route,
+            total_distance=total_distance + distance
+        )
+
+        if (result):
+            return result
+
+    return None
 
 
 def count_routes_by_stops(graph, start_node, end_node, stops, operator="<=", _route=""):
@@ -91,7 +122,7 @@ def count_routes_by_stops(graph, start_node, end_node, stops, operator="<=", _ro
     routes (or trips) with a maximum (operator=="<=") of stops=3.
     """
     _route = _route + start_node
-    count = 0
+    stop_counter = 0
     current_stops = len(_route) - 1  # similar to number of edges
 
     for neighbor in graph.edges[start_node]:
@@ -101,12 +132,12 @@ def count_routes_by_stops(graph, start_node, end_node, stops, operator="<=", _ro
         if neighbor == end_node:
             _route = _route + end_node
             if(eval(str(current_stops) + operator + str(stops))):
-                count += 1
+                stop_counter += 1
                 break
 
-        count += count_routes_by_stops(graph, neighbor, end_node, stops, operator=operator, _route=_route)
+        stop_counter += count_routes_by_stops(graph, neighbor, end_node, stops, operator=operator, _route=_route)
 
-    return count
+    return stop_counter
 
 
 def count_routes_by_max_distance(graph, start_node, end_node, max_distance=30, _route=""):
@@ -132,22 +163,24 @@ def count_routes_by_max_distance(graph, start_node, end_node, max_distance=30, _
 
 
 if __name__ == '__main__':
-    g = Graph("AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7")
+    g = Graph("AZ1, ZX2, XA1, AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7, BA4")
 
     # calculate route distances
-    print(route_distance(g, "A-B-C"))
-    print(route_distance(g, "A-D"))
-    print(route_distance(g, "A-D-C"))
-    print(route_distance(g, "A-E-B-C-D"))
-    print(route_distance(g, "A-E-D"))
+    # print(route_distance(g, "A-B-C"))
+    # print(route_distance(g, "A-D"))
+    # print(route_distance(g, "A-D-C"))
+    # print(route_distance(g, "A-E-B-C-D"))
+    # print(route_distance(g, "A-E-D"))
 
     # count routes recursively between a start node and an end node based on a number of stops
-    print(count_routes_by_stops(g, 'C', 'C', 3, operator="<="))  # a maximum of 3 stops
-    print(count_routes_by_stops(g, 'A', 'C', 4, operator="=="))  # exactly 4 stops
+    # print(count_routes_by_stops(g, 'C', 'C', 3, operator="<="))  # a maximum of 3 stops
+    # print(count_routes_by_stops(g, 'A', 'C', 4, operator="=="))  # exactly 4 stops
+    # print(count_routes_by_stops(g, 'A', 'A', 3, operator="=="))  # exactly 4 stops
 
     # shortest route distance based on Nearest Neighbour Algorithm
-    print(shortest_route_distance(g, 'A', 'C'))
-    print(shortest_route_distance(g, 'B', 'B'))
+    print(shortest_route_distance(g, 'A', 'A'))
+    # print(shortest_route_distance(g, 'A', 'C'))
+    # print(shortest_route_distance(g, 'B', 'B'))
 
     # count routes recursively between a start node and an end node based on a max distance
-    print(count_routes_by_max_distance(g, 'C', 'C', max_distance=30))
+    # print(count_routes_by_max_distance(g, 'C', 'C', max_distance=30))
