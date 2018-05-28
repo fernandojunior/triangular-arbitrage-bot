be3bff9b63eb8daa6ddb53978eb0c6a8373d0b7d
Fernando Felix <fernandojr.ifcg@live.com>
2018-05-28 06:42:17 -0300

    Compute reverse symbol pairs and tickers

--- history.js
+++ history.js
@@ -1,43 +1,91 @@
-const dateFormat = require('dateformat')
 const binance = require('node-binance-api')
 const request = require('request-promise')
 const { promisify } = require('es6-promisify')
 
+const mock = require('./mock')
+const Graph = require('./graph')
+const { findRoutePath } = require('./utils')
+
 const exchangeApis = {
   bittrex: {
-    getTicker: async (currencyPairStr) => {
+    async getTicker (currencyPairStr) {
       const url = `https://bittrex.com/api/v1.1/public/getticker?market=${currencyPairStr}`
       return JSON.parse(await request(url)).result
     }
   },
   binance: {
-    getTicker: (...args) => {
-      return promisify(binance.bookTickers)(...args)
+    getTicker (currencyPairStr) {
+      return promisify(binance.bookTickers)(currencyPairStr)
+    },
+
+    async getTickers () {
+      const tickerList = (await promisify(binance.bookTickers)())
+
+      return tickerList.reduce((acc, { symbol, bidPrice, askPrice }) => {
+        acc[symbol] = { bidPrice: parseFloat(bidPrice), askPrice: parseFloat(askPrice) }
+        return acc
+      }, {})
+    },
+
+    getBalances () {
+      return Promise.resolve(mock.binanceBalances)
+    },
+
+    async getSymbols () {
+      return Object.keys(await this.getBalances())
+    },
+
+    async getSymbolPairMetas () {
+      const symbols = (await this.getSymbols())
+      const pairs = {}
+
+      symbols.forEach(baseSymbol => symbols.forEach(quotaSymbol => {
+        if (baseSymbol !== quotaSymbol) {
+          pairs[baseSymbol + quotaSymbol] = { baseSymbol, quotaSymbol, reversePair: quotaSymbol + baseSymbol }
+        }
+      }))
+
+      return pairs
     }
+
   }
 }
 
-const tickerHistory = []
-const delayTime = 1000
-const maxRequests = 4
-let requestCounter = 0
+exchangeApis.binance.getTickers().then(async (tickers) => {
+  const twoWayTickers = {}
 
-// Bitcoin prices vary depending on the exchange you're buying it on
-// https://www.cnbc.com/2017/12/12/why-bitcoin-prices-are-different-on-each-exchange.html
-const exchangeRequestInterval = setInterval(() => {
-  if (requestCounter === maxRequests) {
-    clearInterval(exchangeRequestInterval)
-  }
+  const allSymbolPairMetas = await exchangeApis.binance.getSymbolPairMetas()
 
-  exchangeApis.binance.getTicker('LTCBTC').then((ticker) => {
-    tickerHistory.push({ time: dateFormat(new Date(), 'UTC:HH:MM:ss.l'), Bid: ticker.askPrice, Ask: ticker.bidPrice, exchange: 'binance' })
+  const tickerSymbolPairs = Object.keys(tickers)
+
+  tickerSymbolPairs.forEach(tickerSymbolPair => {
+    const pairMeta = allSymbolPairMetas[tickerSymbolPair]
+
+    if (pairMeta && !Object.keys(twoWayTickers).includes(pairMeta.reversePair)) {
+      // main way to trade
+      const ticker = tickers[tickerSymbolPair]
+      ticker.isReversePair = false
+      ticker.baseSymbol = pairMeta.baseSymbol
+      ticker.quotaSymbol = pairMeta.quotaSymbol
+      twoWayTickers[tickerSymbolPair] = ticker
+
+      // reverse way to trade
+      twoWayTickers[pairMeta.reversePair] = {
+        bidPrice: 1 / ticker.askPrice,
+        askPrice: 1 / ticker.bidPrice,
+        isReversePair: true,
+        baseSymbol: ticker.quotaSymbol,
+        quotaSymbol: ticker.baseSymbol
+      }
+    }
   })
 
-  exchangeApis.bittrex.getTicker('BTC-LTC').then((ticker) => {
-    tickerHistory.push({ time: dateFormat(new Date(), 'UTC:HH:MM:ss.l'), Bid: ticker.Bid, Ask: ticker.Ask, exchange: 'bittrex' })
+  const pairCosts = new Graph()
+  Object.keys(twoWayTickers).forEach((tickerName) => {
+    const ticker = twoWayTickers[tickerName]
+    pairCosts.addEdge(ticker.baseSymbol, ticker.quotaSymbol, ticker.bidPrice)
   })
 
-  requestCounter++
-}, delayTime)
-
-setTimeout(() => console.log(tickerHistory), delayTime * maxRequests + 1000)
+  // TODO change cost function for findRoutePath function
+  console.log(findRoutePath(pairCosts, 'BTC', 'BTC', false))
+})
--- utils.js
+++ utils.js
@@ -1,36 +1,24 @@
-function findRoutePath (graph, startNode, endNode, shortest = true, currentNode = null, route = '', totalDistance = 0, maxStops = 3) {
+function findRoutePath (graph, startNode, endNode, shortest = true, currentNode = null, route = [], totalDistance = 0, maxStops = 3) {
   currentNode = currentNode || startNode
-  route = route + currentNode
+  route.push(currentNode)
 
   const sortedNeighborDistances = graph.getSortedNeighborDistances(currentNode, !shortest)
 
-  if (route.length > 2 && [startNode, endNode].includes(currentNode)) {
+  if (route.length > 2 && route[0] === startNode && route[route.length - 1] === endNode) {
     return { route, totalDistance }
   }
 
   for (const neighbor of Object.keys(sortedNeighborDistances)) {
     const distance = sortedNeighborDistances[neighbor]
+    const expetedEdgeNumber = [...route, neighbor].length - 1
 
-    if (route.length > maxStops) {
-      continue
-    }
-
-    if (![startNode, endNode].includes(neighbor) && route.includes(neighbor)) {
+    if (![startNode, endNode].includes(neighbor) && (route.includes(neighbor) || expetedEdgeNumber === maxStops)) {
       continue
     }
 
     currentNode = neighbor
 
-    const result = findRoutePath(
-      graph,
-      startNode,
-      endNode,
-      shortest,
-      currentNode,
-      route,
-      totalDistance + distance,
-      maxStops
-    )
+    const result = findRoutePath(graph, startNode, endNode, shortest, currentNode, route, totalDistance + distance, maxStops)
 
     if (result) {
       return result
